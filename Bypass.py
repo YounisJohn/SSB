ff2ca89c44d3a54f4ad4c76fc38e77330e172413
SACRED GAMES 3.05 <106374961+YounisJohn@users.noreply.github.com>
2022-06-25 13:40:09 +0500

    Update Bypass.py

--- Bypass.py
+++ Bypass.py
@@ -4,7 +4,7 @@ b = platform.architecture()[0]
 
 if b == '64bit':
 
-    print("\n\x1b[1;92m Congratulations Your Device Support This Tool\033[1;37m")
+    print("\n\x1b[1;92mCongratulations Your Device Support This Tool\033[1;37m")
 
     os.system('xdg-open https://youtube.com/channel/UC6OSDzf3I3Ws8onwga637uA/');time.sleep(5)
 
