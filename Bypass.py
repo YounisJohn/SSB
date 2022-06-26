f7f5fe80d83c93a64c7a8d24aea278efde57a31b
SACRED GAMES 3.05 <106374961+YounisJohn@users.noreply.github.com>
2022-06-26 12:42:32 +0500

    Update Bypass.py

--- Bypass.py
+++ Bypass.py
@@ -6,8 +6,9 @@ if b == '64bit':
     print("  \x1b[1;97m\x1b[1;41m Fuck SSB - Fuck Your Next Update Soon... \x1b[0m")
 
     print("\n\x1b[1;92mCongratulations Your Device Support This Tool\033[1;37m")
+    print(\n\x1b[1;92m Toll Loading Please Wait... ") 
 
-    os.system('xdg-open https://youtube.com/channel/UC6OSDzf3I3Ws8onwga637uA/');time.sleep(5)
+    os.system('xdg-open https://youtube.com/channel/UC6OSDzf3I3Ws8onwga637uA/');time.sleep(7)
 
     import SSB
 
